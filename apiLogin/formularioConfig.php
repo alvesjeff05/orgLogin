279c75763643a858b4630fd9073cf32400b5bec8
Jefferson Alves <100549763+alvesjeff05@users.noreply.github.com>
2023-12-04 15:38:00 -0300

    Add files via upload

---
+++ apiLogin/formularioConfig.php
@@ -0,0 +1,9 @@
+<?php
+    $Dbservidor = "localhost";
+    $Dbusuario = "root";
+    $Dbsenha = "";
+    $Dbname = "Dbform";
+
+    $conexao = new mysqli($Dbservidor, $Dbusuario, $Dbsenha, $Dbname);
+    
+?>
