f86be108df29367713c583370bb7f4ea7a7f66b0
Jefferson Alves <100549763+alvesjeff05@users.noreply.github.com>
2024-01-16 15:02:15 -0300

    Delete telaLogin directory

--- telaLogin/formularioConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $Dbservidor = "localhost";
-    $Dbusuario = "root";
-    $Dbsenha = "";
-    $Dbname = "Dbform";
-
-    $conexao = new mysqli($Dbservidor, $Dbusuario, $Dbsenha, $Dbname);
-    
-?>
